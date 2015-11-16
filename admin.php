d4ec7d58f939866a920c40ad7e467f981df32509
cfaulconer <cfaulconer@gmail.com>
2015-11-16 11:47:15 -0700

    fixed typo

--- admin.php
+++ admin.php
@@ -7,7 +7,7 @@ echo '
     <h1>Admin Links</h1>
     <a href="load-data.php">Drop and reload the users and guides collections.</a>
     <hr>
-    <a href="show-all.php">Show everhting in the guides and users collections.</a>
+    <a href="show-all.php">Show everything in the guides and users collections.</a>
     <hr>
     <a href="remove.php">Remove all guides in the guides collection.</a>
     ';
